5a0743fa4a4ae10e244661fa5ddc2f58834c2f81
Marcus Vinicius <mvfariadesouza@gmail.com>
2025-10-16 20:00:31 -0300

    Projeto C11

--- main.py
+++ main.py
@@ -2,58 +2,54 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Questão 1 -----------------------------------------------------------------------
-# ds = np.loadtxt('war.csv',
-#                 delimiter=',',
-#                 dtype=str,
-#                 encoding='utf-8')
+ds = pd.read_csv('war.csv', encoding='utf-8')
+# Questão 1 -----------------------------------------------------------------------
+
+# ds_filter = ds[['Country', 'Total bilateral allocations($ billion)']].copy()
 #
-# np.set_printoptions(precision=9, suppress=True) #evita que os numero sejam mostrados em forma de notação cientifica e limita 4 casas decimais
-# newDs = ds[1:, [0,11]] #filtrando as duas colunas
-# newDs[:, 1] = np.where(newDs[:, 1] == '', '0', newDs[:, 1]) #trocando vazio por zero
-# values = newDs[:,1].astype(float) #de string pra float
+# ds_filter['Total bilateral allocations($ billion)'] = ds_filter['Total bilateral allocations($ billion)'].replace('', 0)
+# ds_filter['Total bilateral allocations($ billion)'] = pd.to_numeric(ds_filter['Total bilateral allocations($ billion)'], errors='coerce').fillna(0)
 #
-# indices = values.argsort()[::-1] #pegando os indices ordenados de forma decrescente
-# top5_indices = indices[:5] #pega os 5 maiores índices
-# top5 = newDs[top5_indices] #top 5 maiores países e seus valores
-#
-# print("Top 5 países que mais doaram: ")
+# top5 = ds_filter.sort_values(by='Total bilateral allocations($ billion)', ascending=False).head(5)
+# print("Top 5 países que mais doaram:")
 # print(top5)
 #
-# # Separando nomes e valores
-# countries = top5[:, 0]
-# donations = top5[:, 1].astype(float)
-#
-# #Criando o gráfico de barras
 # plt.figure(figsize=(10, 6))
-# plt.bar(countries, donations, color='skyblue')
-# plt.xlabel('Países')
-# plt.ylabel('Doações (em bilhões)')
-# plt.title('Top 5 países que mais doaram para a Ucrânia (2024)')
+# barras = plt.bar(top5['Country'], top5['Total bilateral allocations($ billion)'], color='green')
+# plt.bar_label(barras, fmt='%.2f', padding=3, fontweight='bold')
+# plt.xlabel('Países', fontweight='bold')
+# plt.ylabel('Doações (em bilhões)', fontweight='bold')
+# plt.title('Top 5 países que mais doaram para a Ucrânia (2024)', fontweight='bold')
 # plt.grid(axis='y', linestyle='--', alpha=0.5)
 # plt.tight_layout()
+# plt.show()
 #
-# # Exibindo o gráfico
+# # Questão 2 –-------------------------------------------------------
+# last5 = ds_filter.sort_values(by='Total bilateral allocations($ billion)', ascending=True).head(5)
+# last5 = last5.iloc[::-1]
+# print("\nTop 5 países que menos doaram:")
+# print(last5)
+#
+# # Gráfico
+# plt.figure(figsize=(10, 6))
+# bars = plt.bar(last5['Country'], last5['Total bilateral allocations($ billion)'], color='red')
+# plt.bar_label(bars, fmt='%.6f', padding=3, fontweight='bold')
+# plt.xlabel('Países', fontweight='bold')
+# plt.ylabel('Doações (em bilhões)', fontweight='bold')
+# plt.title('Top 5 países que menos doaram para a Ucrânia', fontweight='bold')
+# plt.grid(axis='y', linestyle='--', alpha=0.5)
+# plt.tight_layout()
 # plt.show()
 
-#Questão 2 -----------------------------------------------------------
 
-# last5_indices = indices[-5:][::-1] #5 ultimos de forma crescente
-# last5 = newDs[last5_indices] #Top 5 menores países e seus valores
-#
-# print('')
-# print("Top 5 países que menos doaram: ")
-# print(last5)
+# Questão 3----------------------------------------------------------------------------------
 
-#Questão 3----------------------------------------------------------------------------------
+# Questão 4----------------------------------------------------------------------------------
 
-#Questão 4----------------------------------------------------------------------------------
+# Questão 5----------------------------------------------------------------------------------
 
-#Questão 5----------------------------------------------------------------------------------
+# Questão 6----------------------------------------------------------------------------------
 
-#Questão 6----------------------------------------------------------------------------------
-# ds = pd.read_csv('war.csv', delimiter=',')
-#
 # ds_euMember = ds['EU member_x'] == 1
 #
 # contry_EU = ds[ds_euMember]['Country']
@@ -96,25 +92,21 @@ import matplotlib.pyplot as plt
 #     military_NEU.sum()
 # ]
 #
-# # Rótulos para os tipos de alocação
 # categories = ['Financeiro', 'Humanitário', 'Militar']
 # x = range(len(categories))  # [0, 1, 2]
 #
-# # Criar o gráfico
 # bar_width = 0.35
 #
 # plt.figure(figsize=(10, 6))
 # plt.bar([i - bar_width/2 for i in x], eu_values, width=bar_width, label='UE', color='royalblue')
 # plt.bar([i + bar_width/2 for i in x], neu_values, width=bar_width, label='Não UE', color='orange')
 #
-# # Adicionando rótulos
 # plt.xlabel('Tipo de Alocação')
 # plt.ylabel('Total em $ bilhões')
 # plt.title('Comparação de Alocações (UE vs Não UE)')
 # plt.xticks(ticks=x, labels=categories)
 # plt.legend()
 #
-# # Adicionar valores nas barras
 # for i in range(len(x)):
 #     plt.text(i - bar_width/2, eu_values[i] + 0.5, f'{eu_values[i]:.1f}', ha='center')
 #     plt.text(i + bar_width/2, neu_values[i] + 0.5, f'{neu_values[i]:.1f}', ha='center')
@@ -123,37 +115,32 @@ import matplotlib.pyplot as plt
 # plt.grid(axis='y', linestyle='--', alpha=0.6)
 # plt.show()
 
+# Questão 7----------------------------------------------------------------------------------
+
+# Questão 8----------------------------------------------------------------------------------
+
+# Questão 9----------------------------------------------------------------------------------
 
 
 
-#Questão 7----------------------------------------------------------------------------------
-
-#Questão 8----------------------------------------------------------------------------------
-
-#Questão 9----------------------------------------------------------------------------------
-
-#Questão 10----------------------------------------------------------------------------------
-# 1) Top 5 países que mais doaram - feito
+# Questão 10----------------------------------------------------------------------------------
+# ds_UE = ds[ds['EU member_x'] == 1].copy()
+# ds_country = ds_UE[['Country', 'Total bilateral allocations($ billion)']].copy()
 #
-# 2) Top 5 países que menos doaram - feito
+# ds_country['Total bilateral allocations($ billion)'] = ds_country['Total bilateral allocations($ billion)'].replace('', 0)
+# ds_country['Total bilateral allocations($ billion)'] = pd.to_numeric(ds_country['Total bilateral allocations($ billion)'], errors='coerce').fillna(0)
 #
-# 3) Países que prometeram mais do que realmente doaram
+# top5 = ds_country.sort_values(by='Total bilateral allocations($ billion)', ascending=False).head(5)
 #
-# 4) Países que prometeram menos do que realmente doaram
+# print("Top 5 países da União Europeia que mais doaram:")
+# print(top5)
 #
-# 5) Qual a relação do PIB do país com suas doações(porcentagem do PIB)
-#
-# 6) Padrões de Ajuda na União Europeia: Como os membros da União Europeia se comparam aos países não
-# membros em termos de alocações financeiras, humanitárias e militares como proporção do total de suas ajudas
-#
-# 7) Foco da Ajuda: Militar vs. Humanitária: Que países priorizam as alocações militares em
-# detrimento das humanitárias, e vice-versa? Existe alguma tendência regional ou geopolítica observável?
-#
-# 8) países que dão mais ajuda financeira também tendem a dar mais ajuda militar
-#
-# 9) Análise Comparativa de Perfis de Ajuda: É possível agrupar os países em diferentes "perfis de doadores"
-# com base na composição de sua ajuda (por exemplo, "humanitários", "desenvolvimentistas", "estrategistas militares")?
-#
-# 10) 5 países da uniao europeia que mais contribuem
-
-
+# plt.figure(figsize=(10, 6))
+# barras = plt.bar(top5['Country'], top5['Total bilateral allocations($ billion)'], color='green')
+# plt.bar_label(barras, fmt='%.2f', padding=3, fontweight='bold')
+# plt.xlabel('Países', fontweight='bold')
+# plt.ylabel('Doações (em bilhões)', fontweight='bold')
+# plt.title('Top 5 países da União Europeia que mais doaram', fontweight='bold')
+# plt.grid(axis='y', linestyle='--', alpha=0.5)
+# plt.tight_layout()
+# plt.show()
